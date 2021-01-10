c5a2779d7b05075754bfe9d4947d977ebd880614
rjclark2 <johnclarkclemson@gmail.com>
2021-01-10 14:31:57 -0500

    test

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-  return render_template('layout.html')
+def home():
+  return render_template('home.html')
 
 @app.route('/about')
 def about():
